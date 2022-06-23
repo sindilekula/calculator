4c5a9c79c52f50c5e490a76f04649b2e940d625d
sindilekula <Kulasindile@gmail.com>
2022-06-23 12:58:29 +0200

    done2

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 let sum = "";
 
-function buildSum(char){
-    sum += char;
-    document.getElementById("sum").innerHTML = sum;
+function buildSum(char) {
+  sum += char;
+  document.getElementById("sum").innerHTML = sum;
 }
 
-function evaluateSum(){
-    sum = eval(sum);
-    document.getElementById("sum").innerHTML = sum;
+function evaluateSum() {
+  sum = eval(sum);
+  document.getElementById("sum").innerHTML = sum;
 }
 
-function reset(){
-    sum="";
-    document.getElementById("sum").innerHTML = sum;
+function reset() {
+  sum = "";
+  document.getElementById("sum").innerHTML = sum;
 }
